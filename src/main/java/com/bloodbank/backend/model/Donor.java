670121c3950fcf0004b935d9ed5611765eafcd73
aishwaryaugale <aishwaryaugale10@gmail.com>
2026-02-09 20:15:32 +0530

    fix donor

--- src/main/java/com/bloodbank/backend/model/Donor.java
+++ src/main/java/com/bloodbank/backend/model/Donor.java
@@ -1,31 +1,66 @@
 package com.bloodbank.backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "donors")
 public class Donor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "donor_id")
+    private Long donorId;
 
     private String name;
+
+    @Column(name = "blood_group")
     private String bloodGroup;
+
     private String city;
     private String phone;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getDonorId() {
+        return donorId;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setDonorId(Long donorId) {
+        this.donorId = donorId;
+    }
 
-    public String getBloodGroup() { return bloodGroup; }
-    public void setBloodGroup(String bloodGroup) { this.bloodGroup = bloodGroup; }
+    public String getName() {
+        return name;
+    }
 
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
